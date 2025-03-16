fde865ad68c1757330da3f3b9da4ad73bea1da7c
okikuya <noilfo3hcramosa@gmail.com>
2025-03-16 19:58:53 +0900

    いいねの数を表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,7 +16,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!@)/ }
+  validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
@@ -29,16 +29,20 @@ class Article < ApplicationRecord
   belongs_to :user
 
   def display_created_at
-    I18n.l(created_at, format: :default)
+    I18n.l(self.created_at, format: :default)
   end
 
   def author_name
     user.display_name
   end
 
+  def like_count
+    likes.count
+  end
+
   private
   def validate_title_and_content_length
-    char_count = title.length + content.length
+    char_count = self.title.length + self.content.length
     errors.add(:content, '100文字いじょうで！') unless char_count > 100
   end
 end
