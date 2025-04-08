eda40e1a3205ea1879a1ec3cacfa6e8d45b142ef
okikuya <noilfo3hcramosa@gmail.com>
2025-04-08 22:05:25 +0900

    フォームの表示

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -32,6 +32,11 @@ document.addEventListener('DOMContentLoaded', () => {
       handleHeartDisplay(hasLiked)
     })
 
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+
   $('.inactive-heart').on('click', () => {
     axios.post(`/articles/${articleId}/like`)
       .then((response) => {
