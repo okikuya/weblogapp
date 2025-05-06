4b9194fdf4e503f2a4f90a240cd75e50df3f9dcf
okikuya <noilfo3hcramosa@gmail.com>
2025-05-06 09:44:30 +0900

    保存に失敗する場合のテストを追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  let!(:user) do
+    User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+  end
+
   context 'タイトルと内容が入力されている場合' do
-    let!(:user) do
-      User.create!({
-        email: 'test@example.com',
-        password: 'password'
-      })
-    end
 
     let!(:article) do
       user.articles.build({
@@ -18,6 +19,19 @@ RSpec.describe Article, type: :model do
 
     it '記事を保存できる' do
       expect(article).to be_valid
+    end
+  end
+
+  context 'タイトルの文字が一文字の場合' do
+    let!(:article) do
+      user.articles.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+
+    it '記事を保存できない' do
+      expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
   end
 end
